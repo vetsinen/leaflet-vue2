cfc072ed219b88597f62dec185cc703732185c77
vetsinen <vetsinen@gmail.com>
2022-02-02 15:36:53 +0200

    marker fixed

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -8,6 +8,15 @@
 import L from 'leaflet';
 import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
 
+import { Icon } from 'leaflet';
+
+delete Icon.Default.prototype._getIconUrl;
+Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
+
 export default {
   name: "Map",
   data: function () {
